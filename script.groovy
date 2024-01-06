decd77fc59e6ec52cb81e65ef4e8c959872cfff5
ahmed elshnawy <ahmedelshanwy000@gmail.com>
2024-01-06 19:13:35 +0200

    fix bug in groovy script file

--- script.groovy
+++ script.groovy
@@ -1,15 +1,17 @@
 def buildApp() {
     sh 'echo "build the app"'
     withCredentials([usernamePassword(credentialsId: 'dockerhub', 
-            usernameVariable: 'USERNAME', passwordVariable: 'PASS')]) 
-                sh 'docker build -t ahmed0130/todo-app:x3 .'
-                sh 'echo $PASS | docker login -u $USERNAME --password-stdin'  
-                sh 'docker push ahmed0130/todo-app:x3'
+            usernameVariable: 'USERNAME', passwordVariable: 'PASS')]) {
+        sh 'docker build -t ahmed0130/todo-app:x3 .'
+        sh 'echo $PASS | docker login -u $USERNAME --password-stdin'  
+        sh 'docker push ahmed0130/todo-app:x3'
+    }
 }
+
 def testApp() {
- sh ' echo "test the application......" '
+    sh 'echo "test the application......"'
 }
+
 def deployApp() {
- sh 'echo "deploy the application to linode...." '
+    sh 'echo "deploy the application to linode...."'
 }
-return this
