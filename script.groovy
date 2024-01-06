ca5f2c6002752625b9c1a828cf55687aee29460b
ahmed elshnawy <ahmedelshanwy000@gmail.com>
2024-01-06 18:58:24 +0200

    move the configuration to script.groovy file

---
+++ script.groovy
@@ -0,0 +1,15 @@
+def buildApp() {
+    sh 'echo "build the app"'
+    withCredentials([usernamePassword(credentialsId: 'dockerhub', 
+            usernameVariable: 'USERNAME', passwordVariable: 'PASS')]) 
+                sh 'docker build -t ahmed0130/todo-app:x3 .'
+                sh 'echo $PASS | docker login -u $USERNAME --password-stdin'  
+                sh 'docker push ahmed0130/todo-app:x3'
+}
+def testApp() {
+ sh ' echo "test the application......" '
+}
+def deployApp() {
+ sh 'echo "deploy the application to linode...." '
+}
+return this
