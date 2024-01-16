065d067df91d29dbdfd99109bdbc40390a5e728b
ahmed elshnawy <ahmedelshanwy000@gmail.com>
2024-01-16 16:11:22 +0200

    configure sqlite insted of mysql for testing perpose

--- Dockerfile
+++ Dockerfile
@@ -4,4 +4,5 @@ COPY requirements.txt /app/
 RUN pip install --no-cache-dir -r requirements.txt
 COPY . /app/
 EXPOSE 8000
+RUN python manage.py migrate
 CMD ["python", "manage.py", "runserver","0.0.0.0:8000"]
--- todoapp/settings.py
+++ todoapp/settings.py
@@ -80,16 +80,14 @@ WSGI_APPLICATION = 'todoapp.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('DB_NAME'),
-        'USER': os.environ.get('DB_USER'),
-        'PASSWORD': os.environ.get('DB_PASSWORD'),
-        'HOST': os.environ.get('DB_HOST'),
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / "db.sqlite3",
     }
 }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
