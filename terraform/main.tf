70ec6fa03d66eeec4c5bc567e0551683fc538de8
ahmed elshnawy <ahmedelshanwy000@gmail.com>
2024-03-19 09:46:05 +0200

    configured s3 bucket for tf state

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,12 @@
+terraform {
+    backend "s3" {
+      bucket         	   = "app-terraform-state"
+      key              	   = "todo-app/terraform.tfstate"
+      region         	   = "eu-north-1"
+  }
+}
+
+
 provider "aws" {
   region = "eu-north-1"  
 }
