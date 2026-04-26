85862649102ba0ea1cde02c6b340f4bc39875614
Ahmed Elshenawy Elshenawy <ahmedelshenawy@ntgclarity.com>
2026-04-26 19:04:19 +0300

    migrate DB to aws RDS mysql

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -83,8 +83,12 @@ WSGI_APPLICATION = 'todoapp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'todoapp',
+        'USER': 'admin',
+        'PASSWORD': '12345678',
+        'HOST': 'database-1.c3mkiuiysai5.eu-north-1.rds.amazonaws.com',  
+        'PORT': '3306',       
     }
 }
 
